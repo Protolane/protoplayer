1432a31ac9bc408df049d04c4942d70d9bf22103
Bruno Finger <bruno.k.finger@gmail.com>
2016-05-23 10:19:10 +0200

    Bare base

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  rename = require("gulp-rename"),
+  exec = require('child_process').exec;
+
+gulp.task('scss', function() {
+  gutil.log('Generating CSS');
+
+  return gulp.src('scss/multiplayer.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+        style: 'compressed'
+      }).on('error', sass.logError))
+      .pipe(rename('styles.min.css'))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('.'));
+});
+
+gulp.task('default', ['scss'], function() {
+  exec(
+    'electron main.js',
+    function(error, stdout, stderr) {
+      stdout.on('data', function (data) {
+        gutil.log(data);
+      });
+
+      stderr.on('data', function (data) {
+        gutil.log(data);
+      });
+    });
+    gulp.watch('scss/**/*.scss', ['scss']);
+});
